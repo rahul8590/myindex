b9cfd471fba1d3211dbf63faa5e0e1651c30b756
rahul <rahul8590@gmail.com>
2014-10-10 14:21:24 -0400

    Messing with lucene

---
+++ src/mainsearch.java
@@ -0,0 +1,25 @@
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+
+public class mainsearch {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("just trying to search something with the index");
+		Analyzer a; // = new Analyzer(Version.LUCENE_4_0);
+		Directory index = new RAMDirectory();
+		
+		
+		
+			
+		
+
+	}
+
+}
